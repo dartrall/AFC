f45dd2dc433e168cccd593ae513912428affe760
Daria <darktrapall@gmail.com>
2022-05-03 16:44:31 +0100

    JetFuelCalcuktion

---
+++ AFC.BLL/JetFuelCalculation.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AFC.BLL
+{
+    class JetFuelCalculation
+    {
+        private static decimal _usagePerMinute = 58.4m * 2;
+        private static decimal _avgSpeed = 748.7m;
+
+        public decimal CalcuateRequiredFuel(Int64 distance)
+        {
+            return (distance / _avgSpeed * 60 * _usagePerMinute);
+        }
+    }
+}
